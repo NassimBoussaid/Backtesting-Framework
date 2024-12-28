705e4f3d471afab6c2ecbf9d2030ca890831afb9
Hugs263 <hugosoulier1234@gmail.com>
2024-12-28 19:31:43 +0100

    Ajout possibilité de choisir le schéma de pondération (EqualWeight & MarketCapWeight)

--- Core/Backtester.py
+++ Core/Backtester.py
@@ -10,21 +10,36 @@ class Backtester:
     Classe permettant de backtester une stratégie financière sur un ensemble de données.
     """
 
-    def __init__(self, data_source, multi_assets=False, special_start=1, transaction_cost=0.0,
+    def __init__(self, data_source, weight_scheme='EqualWeight', market_cap_source=None,
+                 multi_assets=False, special_start=1, transaction_cost=0.0,
                  slippage=0.0, rebalancing_frequency='monthly'):
         """
         Initialise l'objet Backtester.
 
         :param data_source: Fichier CSV ou DataFrame Pandas contenant les données à backtester.
+        :param weight_scheme: Schéma de pondération à utiliser ('EqualWeight' ou 'MarketCapWeight').
+                              Par défaut 'EqualWeight'.
+        :param market_cap_source: Chemin vers le fichier CSV des capitalisations boursières. Requis si
+                                  weight_scheme est 'MarketCapWeight'.
+        :param multi_assets: Booléen indiquant si le backtest concerne plusieurs actifs simultanément.
         :param special_start: Indice à partir duquel le backtest commence.
         :param transaction_cost: Montant des coûts de transaction par rebalancement (par défaut : 0.0).
-        :param slippage: Montant des coûts de slippage (exécution différente de l'ordre) par rebalancement (par défaut : 0.0).
+        :param slippage: Montant des coûts de slippage (exécution différente de l'ordre) par rebalancement
+                         (par défaut : 0.0).
+        :param rebalancing_frequency: Fréquence de rebalancement ('monthly', 'weekly', etc.).
         """
         self.data = load_data(data_source)
-        self.special_start = special_start
+        self.weight_scheme = weight_scheme
+        self.market_cap_source = market_cap_source
         self.multi_assets = multi_assets
+        self.special_start = special_start
         self.transaction_cost = transaction_cost
         self.slippage = slippage
+
+        # Charger et aligner les données de capitalisation boursière si nécessaire
+        if self.weight_scheme == 'MarketCapWeight':
+            self.market_caps = None
+            self.load_market_caps()
 
         # Détermination des bornes pour le calendrier
         self.start_date = self.data.index[0].strftime('%Y-%m-%d')
@@ -36,6 +51,29 @@ class Backtester:
             end_date=self.end_date
         )
 
+        # Initialisation de la weight_matrix
+        self.weight_matrix = None
+
+    def load_market_caps(self):
+        """
+        Charge les données de capitalisation boursière et les aligne avec les données de marché.
+        """
+        if self.market_cap_source is None:
+            raise ValueError("market_cap_source doit être fourni si weight_scheme est 'MarketCapWeight'")
+
+        self.market_caps = load_data(self.market_cap_source)
+
+        # Réindexer avec self.data + forward filling si data manquante
+        if not self.market_caps.index.equals(self.data.index):
+            self.market_caps = self.market_caps.reindex(self.data.index).ffill()
+
+        # Réindexer avec self.data en gardant uniquement l'intersection
+        common_columns = self.data.columns.intersection(self.market_caps.columns)
+        if common_columns.empty:
+            raise ValueError(
+                "Il n'y a aucune colonne en commun entre les données de marché et les capitalisations boursières.")
+        self.market_caps = self.market_caps[common_columns]
+
     def run(self, strategy: Strategy):
         """
         Exécute la stratégie donnée sur les données de marché.
@@ -44,6 +82,7 @@ class Backtester:
         :return: Instance de la classe Result contenant les résultats du backtest.
         """
         composition_matrix = self.calculate_composition_matrix(strategy)
+        self.weight_matrix = self.calculate_weight_matrix(composition_matrix)
         asset_contributions, portfolio_returns, cumulative_asset_returns, cumulative_returns, result_trade = \
             self.calculate_returns(composition_matrix)
 
@@ -97,6 +136,25 @@ class Backtester:
                     composition_matrix.at[current_date, asset] = current_position
 
         return composition_matrix
+
+    def calculate_weight_matrix(self, composition_matrix):
+        """
+        Calcule la matrice des pondérations du portefeuille au cours du temps en fonction du schéma de pondération.
+
+        :param composition_matrix: DataFrame des positions du portefeuille.
+        :return: DataFrame Pandas représentant les pondérations du portefeuille.
+        """
+        if self.weight_scheme == 'EqualWeight':
+            selected_counts = composition_matrix.abs().sum(axis=1).replace(0, pd.NA)
+            weight_matrix = composition_matrix.divide(selected_counts, axis=0).fillna(0)  # Normalisation des poids
+        elif self.weight_scheme == 'MarketCapWeight':
+            weighted_market_caps = composition_matrix * self.market_caps
+            sum_market_caps = weighted_market_caps.abs().sum(axis=1).replace(0, pd.NA)
+            weight_matrix = weighted_market_caps.divide(sum_market_caps, axis=0).fillna(0)  # Normalisation des poids
+        else:
+            raise ValueError(f"Schéma de pondération inconnu : {self.weight_scheme}")
+
+        return weight_matrix
 
     def calculate_transaction_costs(self, shifted_positions):
         """
@@ -154,17 +212,15 @@ class Backtester:
         asset_returns = self.data.pct_change().fillna(0)
 
         # Décalage des positions pour éviter un biais : les positions utilisées en t sont décidées en t-1
-        shifted_positions = composition_matrix.shift(1).fillna(0)
-
-        # Normalisation des positions pour que la somme absolue des poids soit égale à 1
-        shifted_positions = shifted_positions.div(shifted_positions.abs().sum(axis=1), axis=0).fillna(0)
+        weighted_position = composition_matrix * self.weight_matrix
+        shifted_weights = weighted_position.shift(1).fillna(0)
 
         # Calcul des coûts de transaction et de slippage
-        transaction_costs = self.calculate_transaction_costs(shifted_positions)
-        slippage_costs = self.calculate_slippage_costs(shifted_positions)
+        transaction_costs = self.calculate_transaction_costs(shifted_weights)
+        slippage_costs = self.calculate_slippage_costs(shifted_weights)
 
         # Contribution de chaque actif au portefeuille
-        asset_contributions = shifted_positions.multiply(asset_returns, axis=0)
+        asset_contributions = shifted_weights.multiply(asset_returns, axis=0)
 
         # Rendements du portefeuille
         portfolio_returns = asset_contributions.sum(axis=1) - transaction_costs - slippage_costs
@@ -174,12 +230,12 @@ class Backtester:
         cumulative_returns = (1 + portfolio_returns).cumprod() - 1
 
         if self.special_start != 1:
-            shifted_positions = shifted_positions.iloc[self.special_start + 1:]
+            shifted_weights = shifted_weights.iloc[self.special_start + 1:]
             asset_contributions = asset_contributions.iloc[self.special_start + 1:]
             portfolio_returns = portfolio_returns.iloc[self.special_start + 1:]
             cumulative_asset_returns = cumulative_asset_returns.iloc[self.special_start + 1:]
             cumulative_returns = cumulative_returns.iloc[self.special_start + 1:]
 
-        result_trade = self.evaluate_trade(shifted_positions)
+        result_trade = self.evaluate_trade(shifted_weights)
 
         return asset_contributions, portfolio_returns, cumulative_asset_returns, cumulative_returns, result_trade
