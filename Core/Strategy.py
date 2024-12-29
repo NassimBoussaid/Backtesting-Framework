da0a70eb6040e6e1c437730ab199aa4e4558a4ec
nnayr <nassim.boussaid@dauphine.eu>
2024-12-29 13:41:03 +0100

    Package building via Poetry

--- Core/Strategy.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-
-class Strategy(ABC):
-    @abstractmethod
-    def get_position(self, historical_data, current_position):
-        pass
-
-    def fit(self, data):
-        pass
